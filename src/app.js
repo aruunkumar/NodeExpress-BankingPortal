4ffcf1ec088fe8d322de32958cab27a692402775
Aruun Kumar <aruunkumar@gmail.com>
2021-02-18 18:02:03 -0500

    Module1 fix

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-const app = express();
+const app = new express();
 
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));//app.use('/assets',path.join(__dirname,'/public'));
+app.use(express.static(path.join(__dirname, '/public')));//app.use('/assets',path.join(__dirname,'/public'));
 
 app.get('/', (req,res) => { 
 	console.log('Req received'); 
